89b46ba88fa586732ca8db0b3e8c79f0145541a1
shapled <shapled@shapled.com>
2024-08-30 22:50:28 +0800

    change index

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,32 +1,99 @@
-import { Flex, App as AntdApp } from "antd";
+import { Flex, App as AntdApp, Button } from "antd";
 import { Header } from "./core/Header";
 import { PeerClient } from "./core/PeerClient";
 import { PeerServer } from "./core/PeerServer";
-import styled from "styled-components";
+import { css } from "@emotion/css";
+import React, { useState } from "react";
 
-const Container = styled(Flex)`
-  height: 100vh;
-  width: 100%;
-  max-width: 720px;
-  margin: 0 auto;
-`
+const CardButton = (props: { borderColor: string, bgColor: string, children: React.ReactNode }) => {
+  const [hover, setHover] = useState(false);
 
-const Content = styled(Flex)`
-  padding: 0 8px 16px 8px;
-  flex-grow: 1;
-`
+  return (
+    <div className={css`
+      background-color: ${props.borderColor};
+      border-radius: 16px;
+      height: 208px;
+      width: 100%;
+      position: relative;
+    `}>
+      <Flex 
+        justify="center"
+        align="center"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        className={css`
+          background-color: ${props.bgColor};
+          border-radius: 8px;
+          border: none;
+          font-size: 20px;
+          color: white;
+          cursor: pointer;
+          position: absolute;
+          top: ${ hover ? 0 : 64 }px;
+          bottom: ${ hover ? 0 : 64 }px;
+          left: ${ hover ? 0 : 64 }px;
+          right: ${ hover ? 0 : 64 }px;
+          transition: top 0.3s, bottom 0.3s, left 0.3s, right 0.3s;
+        `}
+      >
+        <span className={css`
+          font-size: 50px;
+          font-weight: bold;
+          background: linear-gradient(270deg, #ff0000, #ff7f00, #ffff00, #00ff00, #00ffff, #0000ff, #7f00ff);
+          background-size: 400% 400%;
+          color: transparent;
+          -webkit-background-clip: text;
+          background-clip: text;
+          animation: rainbow-animation 300s ease infinite;
+
+          @keyframes rainbow-animation {
+            0% {
+              background-position: 0% 50%;
+            }
+            50% {
+              background-position: 100% 50%;
+            }
+            100% {
+              background-position: 0% 50%;
+            }
+          }
+        `}>
+          {props.children}
+        </span>
+      </Flex>
+    </div>
+  )
+}
 
 export default function App() {
   const sid = new URLSearchParams(window.location.search).get("sid");
 
   return (
     <AntdApp>
-      <Container vertical>
+      <Flex vertical className={css`
+        height: 100vh;
+        width: 100%;
+        max-width: 720px;
+        margin: 0 auto;  
+      `}>
         <Header />
-        <Content vertical justify="center" align="center">
+        {/* <Flex vertical justify="center" align="center" className={css`
+          padding: 0 8px 16px 8px;
+          flex-grow: 1;  
+        `}>
           {sid ? <PeerClient sid={sid!} />  : <PeerServer />}
-        </Content>
-      </Container>
+        </Flex> */}
+        <div>
+          <CardButton borderColor="#ffedb1" bgColor="#ffd54f">I'm a server</CardButton>
+          <CardButton borderColor="#b1e2ff" bgColor="#4fc3f7">I'm a client</CardButton>
+          {/* <div className={css`
+            flex-grow: 1;
+          `}>I'm a server.</div>
+          <div className={css`
+            flex-grow: 1;
+          `}>I'm a client.</div> */}
+        </div>
+      </Flex>
     </AntdApp>
   );
 }
--- ui/core/Logo.tsx
+++ ui/core/Logo.tsx
@@ -1,72 +1,50 @@
 import { Space } from 'antd';
-import styled from 'styled-components';
-
-const Container = styled(Space)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  user-select: none;
-`;
-
-const TextSection = styled.div`
-  height: 36px;
-  /* background-color: #f4f4f4; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-  font-weight: bold;
-  color: #333;
-  /* border-radius: 10px; */
-  padding: 8px 16px;
-  /* border: 1px solid #ccc; */
-`;
-
-const BlocksSection = styled.div`
-  /* width: 50%; */
-  height: 36px;
-  width: 36px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  grid-gap: 4px;
-`;
-
-const GridItem = styled.div`
-  background-color: #e0e0e0;
-  border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Block1 = styled(GridItem)`
-  background-color: #f5f5f5;
-`;
-
-const Block2 = styled(GridItem)`
-  background-color: #e6e6e6;
-`;
-
-const Block3 = styled(GridItem)`
-  background-color: #d9d9d9;
-`;
-
-const Block4 = styled(GridItem)`
-  background-color: #cccccc;
-`;
+import { css } from '@emotion/css'
 
 export const Logo = () => {
   return (
-    <Container>
-      <BlocksSection>
-        <Block2></Block2>
-        <Block3></Block3>
-        <Block1></Block1>
-        <Block4></Block4>
-      </BlocksSection>
-      <TextSection>QRChat</TextSection>
-    </Container>
+    <Space className={css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      user-select: none;
+    `}>
+      <div className={css`
+        /* width: 50%; */
+        height: 36px;
+        width: 36px;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        grid-gap: 4px;
+
+        & > div {
+          background-color: #e0e0e0;
+          border-radius: 5px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}>
+        <div className={css`background-color: #e6e6e6;`} />
+        <div className={css`background-color: #d9d9d9;`} />
+        <div className={css`background-color: #f5f5f5;`} />
+        <div className={css`background-color: #cccccc;`} />
+      </div>
+      <div className={css`
+        height: 36px;
+        /* background-color: #f4f4f4; */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 24px;
+        font-weight: bold;
+        color: #333;
+        /* border-radius: 10px; */
+        padding: 8px 16px;
+        /* border: 1px solid #ccc; */  
+      `}>QRChat</div>
+    </Space>
   );
 };
